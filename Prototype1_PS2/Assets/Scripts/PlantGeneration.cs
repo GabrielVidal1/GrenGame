1ee39d16cb0a19a2d7d509a43e7942d8c4bc5975
GabrielVidal1 <gvidalayrinhac@gmail.com>
2018-01-24 22:39:41 +0100

    add packages
    - post processing
    - probuilder

---
+++ Prototype1_PS2/Assets/Scripts/PlantGeneration.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantGeneration : MonoBehaviour {
+
+	public float initialRadius;
+	public Vector3 initalDirection;
+
+	public int nbOfSides;
+
+	public AnimationCurve radiusOverLength;
+
+
+	public List<Vector3> points;
+
+	public List<Vector3> faces;
+
+
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/*
+	public void GenerateMesh()
+	{
+		Vector3 u = Vector3.Dot (initalDirection, Vector3.up);
+		Vector3 v = Vector3.Dot (initalDirection, u);
+
+		Vector3 pos = Vector3.zero;
+
+		for (int i = 0; i < nbOfSides; i++) {
+
+
+
+
+
+		}
+
+
+
+
+
+	}
+*/
+}
