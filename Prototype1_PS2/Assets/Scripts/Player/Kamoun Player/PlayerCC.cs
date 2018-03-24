cc11e2e95e84fbb87ff8edfb5071b4689cb47366
GabrielVidal1 <gvidalayrinhac@gmail.com>
2018-03-24 16:39:48 +0100

    Revert "Move during flight"
    This reverts commit 62c91a32c27d07a13fdb537aa965acd5935648e0.

--- Prototype1_PS2/Assets/Scripts/Player/Kamoun Player/PlayerCC.cs
+++ Prototype1_PS2/Assets/Scripts/Player/Kamoun Player/PlayerCC.cs
@@ -7,7 +7,7 @@ public class PlayerCC : MonoBehaviour
 
 	public float Speed = 6f;
 	public float jumpSpeed = 8f;
-	public float gravity = 35f;
+	public float gravity = 20f;
 	private Vector3 moveDirection = Vector3.zero;
 	CharacterController CC;
 	public GameObject camera01;
@@ -29,24 +29,16 @@ public class PlayerCC : MonoBehaviour
 			Screen.lockCursor = true;
 		*/
 		if (!CanvasManager.cm.inGameMenu.isPaused) {
-			if (CC.isGrounded)
-			{
-				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-				moveDirection = transform.TransformDirection(moveDirection);
+			if (CC.isGrounded) {
+				moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+				moveDirection = transform.TransformDirection (moveDirection);
 				moveDirection *= Speed;
 
-				if (Input.GetButtonDown("Jump"))  //if grounded, speed is normal
-				{
+				if (Input.GetButtonDown ("Jump")) {
 					moveDirection.y = jumpSpeed;
 				}
 			}
-			else
-			{
-				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-				moveDirection = transform.TransformDirection(moveDirection); //So the player can move during flight (with speed divided by 2)
-				moveDirection *= Speed/2;
-			}
-
+		
 			moveDirection.y -= gravity * Time.deltaTime;
 			//transform.rotation = Quaternion.Euler(camera01.GetComponent<Mouselook>().xRotation, camera01.GetComponent<Mouselook>().yRotation, 0);
 			transform.rotation = Quaternion.Euler (0, camera01.GetComponent<Mouselook> ().yRotation, 0);
