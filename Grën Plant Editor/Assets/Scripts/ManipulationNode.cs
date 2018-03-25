cd5abc1cceb26f5bcdd5746e9db9c2cdab92bb92
GabrielVidal1 <gvidalayrinhac@gmail.com>
2018-03-25 18:08:45 +0200

    Plant Editor
    Started Plant editor
    - Camera movements
    - initial Radius Change
    - Shape over length Change
    Still some bugs

---
+++ Grën Plant Editor/Assets/Scripts/ManipulationNode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManipulationNode : MonoBehaviour {
+
+	public int nodeIndex;
+
+	public Vector3 axe;
+
+	public NodeType nodeType;
+
+	[SerializeField] private Material activeMaterial;
+	[SerializeField] private Material inactiveMaterial;
+
+	private MeshRenderer mr;
+	private Collider col;
+	void Start()
+	{
+		mr = GetComponent<MeshRenderer> ();
+		mr.material = inactiveMaterial;
+		col = GetComponent<Collider> ();
+	}
+
+	public void SetActiveColor()
+	{
+		mr.material = activeMaterial;
+	}
+
+	public void SetInactiveColor()
+	{
+		mr.material = inactiveMaterial;
+	}
+
+	public void EnableCollider()
+	{
+		col.enabled = true;
+	}
+
+	public void DisableCollider()
+	{
+		col.enabled = false;
+	}
+}
