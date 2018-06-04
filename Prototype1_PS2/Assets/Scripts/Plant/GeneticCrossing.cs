6d04c76e1366c9c0b73de75bd004f18bcd15b98c
GabrielVidal1 <gvidalayrinhac@gmail.com>
2018-06-04 12:41:34 +0200

    Updated Moss Shader
    Primitive Genetic crossing

--- Prototype1_PS2/Assets/Scripts/Plant/GeneticCrossing.cs
+++ Prototype1_PS2/Assets/Scripts/Plant/GeneticCrossing.cs
@@ -4,12 +4,48 @@ using UnityEngine;
 
 public class GeneticCrossing : MonoBehaviour {
 
+	public GeneticParameterGroup[] trunkGenesGroups;
+
+	public Plant defaultPlant;
 
 
+	public GameObject GameObjectWithRenderer(string name)
+	{
+		return new GameObject (name, typeof(MeshFilter), typeof(MeshRenderer));
+	}
 
 
+	public Plant CrossPlants(Plant p1, Plant p2, int randomSeed)
+	{
+		GameObject parent = new GameObject ("PlantParent");
+		Plant trunk = (Plant)Instantiate (defaultPlant, parent.transform);
 
+		foreach (GeneticParameterGroup group in trunkGenesGroups) {
 
+			foreach (string propertyName in group.parameters) {
+				
+				object value = null;
 
+				if (RandomBool ())
+					value = typeof(Plant).GetProperty (propertyName).GetValue (p1, null);
+				else
+					value = typeof(Plant).GetProperty (propertyName).GetValue (p2, null);
+				
+				typeof(Plant).GetProperty (propertyName).SetValue (trunk, value, null);
+			}
+		}
 
+		return trunk;
+	}
+
+	public bool RandomBool()
+	{
+		return Random.value < 0.5f;
+	}
+
+}
+
+public struct GeneticParameterGroup
+{
+	public string[] parameters;
 }
